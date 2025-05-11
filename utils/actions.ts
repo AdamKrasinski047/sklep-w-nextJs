4b871b86acff3db01c17c8d0e1979faf3b4a5b5c
Adam Krasinski <2014adam04@gmail.com>
2025-05-11 20:06:47 +0200

    Initial commit

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,5 +1,17 @@
 import db from '@/utils/db';
+import { redirect } from 'next/navigation';
 
+export const fetchSingleProduct = async (productId: string) => {
+  const product = await db.product.findUnique({
+    where: {
+      id: productId,
+    },
+  });
+  if (!product) {
+    redirect('/products');
+  }
+  return product;
+};
 export const fetchFeaturedProducts = async()=>{
 const produkty = await db.product.findMany({
     where:{
@@ -9,8 +21,14 @@ const produkty = await db.product.findMany({
 return produkty;
 }
 
-export const fetchAllProducts = () => {
+export const fetchAllProducts = ({ search = '' }: { search: string }) => {
   return db.product.findMany({
+    where: {
+      OR: [
+        { name: { contains: search, mode: 'insensitive' } },
+        { company: { contains: search, mode: 'insensitive' } },
+      ],
+    },
     orderBy: {
       createdAt: 'desc',
     },
